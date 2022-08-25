739c4029bd0d79571edee7e01df49e99512f5c65
Averi <averi.sherman@gmail.com>
2022-08-24 20:31:48 -0400

    declared variables in js

---
+++ assets/js/script.js
@@ -0,0 +1,27 @@
+// VARIABLES
+var welcome = document.querySelector("#introduction");
+var startBtn = document.querySelector("#start");
+var introPage =document.querySelector("#intro");
+
+var questionPage = document.querySelector("#questionSec");
+var askQuestion = document.querySelector("#askQuestion");
+
+var reactButtons = document.querySelectorAll(".choices");
+var answerBtn1 = document.querySelector("#ansbtn1");
+var answerBtn2 = document.querySelector("#ansbtn2");
+var answerBtn3 = document.querySelector("#ansbtn3");
+var answerBtn4 = document.querySelector("#ansbtn4");
+
+var checkLine = document.querySelector("#check");
+var scoreBoard = document.querySelector("#submitForm");
+var finalScore = document.querySelector("#finalScore");
+var userInitial =document.querySelector("#initial");
+
+var submitBtn =document.querySelector("#submitBtn");
+var highScorePage =document.querySelector("#hiScore");
+var scoreRecord =document.querySelector("#hiRecords");
+var scoreCheck =document.querySelector("#highScores");
+var finish =document.querySelector("#finish");
+
+var backBtn =document.querySelector("#backBtn");
+var clearBtn=document.querySelector("#clearBtn");
